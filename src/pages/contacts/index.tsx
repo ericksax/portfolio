1511b2dadd1a22746f25518dda8e16b433395b20
ericksax <erickfreitas@gmail.com>
2022-03-08 09:58:42 -0300

    put content on copntacts page

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -1,4 +1,5 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading, Text, Link, HStack } from "@chakra-ui/react";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 import { ContainerBox } from "../../components/ContainerBox";
 import { colors } from "../../styles/colors";
 
@@ -15,6 +16,29 @@ export default function Contacts() {
           Contatos
         </Text>
       </Heading>
+      <Text px="8">
+        Estou presente nas principais redes sociais, ou se preferir pode me
+        contactar por Email. Deixo também meu{" "}
+        <Link
+          color="gray.300"
+          isExternal
+          href="https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:ae93611a-6864-3062-b9e7-cd9667755767"
+        >
+          currículo
+        </Link>{" "}
+        para apreciacao.
+      </Text>
+      <HStack p="8" spacing={30}>
+        <Link>
+          <FaLinkedin size={40} />
+        </Link>
+        <Link>
+          <FaGithub size={40} />
+        </Link>
+        <Link>
+          <FaInstagram size={40} />
+        </Link>
+      </HStack>
     </ContainerBox>
   );
 }
