0203a98686599429c6c220c1db268d63ce408745
ericksax <erickfreitas@gmail.com>
2023-12-19 22:44:11 -0300

    fix: recreating yarn.lock and exclude package-lock.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
       <Box>
         <Saudation />
         <Text py="4" px="8" textAlign="justify" style={{ textIndent: 20 }}>
-          Sou um desenvolvedor front-end em ascenção. Aqui você vai encontrar um
+          Sou um desenvolvedor front-end em ascensão. Aqui você vai encontrar um
           pouco da minha trajetória em busca do conhecimento e de uma construção
           de carreira no mundo das tecnologias web, pelas quais eu sou
           apaixonado.
